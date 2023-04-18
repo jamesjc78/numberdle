927364d07b676efdbf1c02e8bb71031a75e36ae6
jamesjc78 <jla@noaya.no>
2023-04-18 15:06:37 +0800

    config

---
+++ src/components/header/Header.vue
@@ -0,0 +1,5 @@
+<template>
+  <header class="content-center p-4 border-b border-gray-500">
+    <h1 class="font-black text-4xl">Numberdle</h1>
+  </header>
+</template>
---
+++ src/pages/home/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-gray-900 h-screen">
+    <Header />
+  </div>
+</template>
+
+<script lang="ts">
+import Header from "@/components/header/Header.vue";
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
